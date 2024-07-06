eb8f00ea232956719a09f8e45aec3fd7766c649d
piter11251 <duszeq13@gmail.com>
2024-07-06 17:38:38 +0200

    Index page working

---
+++ Auctions/Auctions/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Auctions.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser? User { get; set; }
+        public int? ListingId { get; set; }
+        [ForeignKey("ListingId")]
+        public Listing? Listing { get; set; }
+    }
+}
